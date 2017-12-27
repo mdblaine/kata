35b21b093dc858e31e81d06fa0449e565867fb37
Myron Blaine <mdblaine@yahoo.com>
2017-12-27 08:51:37 -0700

    Bowling test setup. Initial commit.

--- test/audition_test.py
+++ test/audition_test.py
@@ -5,12 +5,20 @@ import os.path
 # Import application code here ...
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.audition import it_runs  # noqa
+from src.bowling import get_score  # noqa
 
 
 class TestMarkdownPy(unittest.TestCase):
 
     def test_it_runs(self):
         self.assertTrue(it_runs())
+
+    def test_bowling(self):
+        self.assertEqual(get_score('X X X X X X X X X X X X'), 300)
+        self.assertEqual(get_score('X X X X X X X X X XXX'), 300)
+        self.assertEqual(get_score('9- 9- 9- 9- 9- 9- 9- 9- 9- 9-'), 90)
+        self.assertEqual(get_score('5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5'), 150)
+        self.assertEqual(get_score('81 -9 2/ X 63 7- 52 X -6 2/X'), 122)
 
 
 if __name__ == '__main__':

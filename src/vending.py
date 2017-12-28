247f0ac64d17192508f7f10daa9af6bd1848ea55
Myron Blaine <mdblaine@yahoo.com>
2017-12-28 11:19:37 -0700

    VendingMachine code commit. Tests pass.

--- src/vending.py
+++ src/vending.py
@@ -9,47 +9,117 @@ class VendingMachine:
 
     def __init__(self):
         self.display = "INSERT COIN"
-        self.display_update_next_check = True
+        self.display_current = False
         self.amount_inserted = 0
-        self.coins_inserted = {"quarter": 0, "dime": 0, "nickle": 0}
-        self.coin_stock = {"quarter": 0, "dime": 0, "nickle": 0}
-        self.product_price = {"cola": 1.00, "chips": 0.50, "candy": 0.65}
+        self.coins_inserted = {"quarters": 0, "dimes": 0, "nickles": 0}
+        self.coin_stock = {"quarters": 0, "dimes": 0, "nickles": 0}
+        self.product_price = {"cola": 100, "chips": 50, "candy": 65}
         self.product_stock = {"cola": 0, "chips": 0, "candy": 0}
 
         Coin = namedtuple("Coin", "name amount weight diameter thickness")
         # see https://www.usmint.gov/learn/coin-and-medal-programs/coin-specifications
-        self.coin_specs = (Coin("quarter", 0.25, 5.670, 24.26, 1.75),
-                           Coin("dime",    0.10, 2.268, 17.91, 1.35),
-                           Coin("nickle",  0.05, 5.000, 21.21, 1.95))
+        self.coin_specs = (Coin("quarters", 25, 5.670, 24.26, 1.75),
+                           Coin("dimes",    10, 2.268, 17.91, 1.35),
+                           Coin("nickles",   5, 5.000, 21.21, 1.95))
 
-    def accept_coins(self):
-        # TODO identify coin by weight and size...then assign value for insert
-        pass
+        self.coins_to_return = {"quarters": 0, "dimes": 0, "nickles": 0}  # only for testing make change
 
-    def select_product(self):
-        # TODO self.display = "THANK YOU" and dispensed
-        # TODO self.display = "SOLD OUT" if no stock; display_update_next_check=False
-        # TODO self.display = "PRICE: ${0:.2f}" if amount not enough; display_update_next_check=False
-        # TODO display = "EXACT CHANGE ONLY"  # not able to make change
-        pass
+    def accept_coin(self, coin):
+        """
+        Add a coin to coins_inserted if in coin_specs
+        :param coin: a dict of the form {"weight": 5.670, "diameter": 24.26, "thickness": 1.75}
+        """
+        # TODO validate coin dict
+        for coin_spec in self.coin_specs:
+            if coin_spec[2:] == (coin["weight"], coin["diameter"], coin["thickness"]):
+                # TODO allow inserted coin to be close enough (e.g. within 3% ???)
+                # a valid coin
+                self.coins_inserted[coin_spec.name] += 1  # TODO not working???
+                self.amount_inserted += coin_spec.amount
+            else:
+                # must not have been a valid coin...passed to coin return
+                pass
 
-    def make_change(self):
-        pass
+    def select_product(self, product):
+        """
+        Respond to product select based on present state
+        :param product: one of: "cola", "chips" or "candy"
+        """
+        # TODO validate product
+        if self.product_stock[product] == 0:
+            self.display = "SOLD OUT"
+        elif self.amount_inserted < self.product_price[product]:
+            self.display = "PRICE: ${0:.2f}".format(self.product_price[product]/100)
+        else:
+            have_change, coins_to_return = self.make_change(self.product_price[product])
+            print("have_change: {}   //  coins_to_return: {}".format(have_change, coins_to_return))
+            if have_change:
+                self.product_stock[product] -= 1  # dispense product...
+                self.coins_to_return = coins_to_return
+                for coin in ["quarters", "dimes", "nickles"]:
+                    self.coin_stock[coin] += self.coins_inserted[coin] - coins_to_return[coin]
+                    # send coins_to_return[coin] to coin return...
+                    self.display = "THANK YOU"
+                    self.amount_inserted = 0
+            elif not have_change:
+                self.display = "EXACT CHANGE ONLY"
+            else:
+                print("SELECT PRODUCT ERROR")
+                self.display = "ERROR"
+        self.display_current = True
+
+    def make_change(self, product_cost):
+        remainder_to_return = self.amount_inserted - product_cost
+        coins_to_return = {"quarters": 0, "dimes": 0, "nickles": 0}
+
+        for coin, coin_amount in [("quarters", 25), ("dimes", 10), ("nickles", 5)]:
+            number_of_coins = remainder_to_return // coin_amount
+            if number_of_coins <= self.coin_stock[coin] + self.coins_inserted[coin]:
+                # have enough of this coin; take number of coins needed; keep track of remainder
+                coins_to_return[coin] = number_of_coins
+                remainder_to_return %= coin_amount
+            else:
+                # don't have enough of this coin; take number of coins that there are; keep track of remainder
+                coins_to_return[coin] = self.coin_stock[coin] + self.coins_inserted[coin]
+                remainder_to_return -= coins_to_return[coin] * coin_amount
+
+        return remainder_to_return == 0, coins_to_return
 
     def return_coins(self):
-        pass
+        """return coins customer has placed in the machine"""
+        # dump all coins_inserted to coin return...
+        self.coins_inserted = {"quarters": 0, "dimes": 0, "nickles": 0}
+        self.amount_inserted = 0
+        self.display = "INSERT COIN"
+        self.display_current = False
 
-    def add_product_stock(self):
-        pass
+    def add_product_stock(self, products):
+        """Add products to product_stock"""
+        # TODO validate products dict
+        for product in products:
+            self.product_stock[product] += products[product]
+        self.display_current = False
+        self.display = "INSERT COIN"
 
     def check_display(self):
-        # TODO display = "INSERT COIN"  # able to make change
-        # TODO display = "EXACT CHANGE ONLY"  # not able to make change
-        # TODO display = "AMOUNT: ${0:.2f}"
-        pass
+        """return appropriate display_current state"""
+        if self.display_current:
+            self.display_current = False
+            return self.display
+        elif self.amount_inserted == 0:
+            self.display = "INSERT COIN"
+            return self.display
+        elif self.amount_inserted > 0:
+            self.display = "AMOUNT: ${0:.2f}".format(self.amount_inserted/100)
+            return self.display
+        else:
+            print("CHECK DISPLAY ERROR")
+            self.display = "ERROR"
+            return self.display
 
 
 def main():
+    # TODO trivial examples of VendingMachine
     pass
 
 
--- test/vending_test.py
+++ test/vending_test.py
@@ -13,29 +13,34 @@ class TestVending(unittest.TestCase):
     def setUp(self):
         self.vending_machine = VendingMachine()
 
-    def test_select_product_initial_conditions(self):
-        for product in [("cola", 1.0), ("chips", 0.5), ("candy", 0.65)]:
-            self.assertEqual(self.vending_machine.select_product(product[0]),
-                             "PRICE: ${0:.2f}".format(product[1]))
-
-    def test_get_products_initial_consitions(self):
-        for quarters in range(7):
-            self.vending_machine.accept_coins({"weight": 5.670, "diameter": 24.26, "thickness": 1.75})
-            self.assertEqual(self.vending_machine.check_display(), "AMOUNT: ${0:.2f}".format(
-                0.25*quarters))
-        for dimes in range(7):
-            self.vending_machine.accept_coins({"weight": 2.268, "diameter": 17.91, "thickness": 1.35})
-            self.assertEqual(self.vending_machine.check_display(), "AMOUNT: ${0:.2f}".format(
-                0.25*quarters + 0.10*dimes))
-        for nickles in range(7):
-            self.vending_machine.accept_coins({"weight": 5.000, "diameter": 21.21, "thickness": 1.95})
-            self.assertEqual(self.vending_machine.check_display(), "AMOUNT: ${0:.2f}".format(
-                0.25*quarters + 0.10*dimes + 0.05*nickles))
+    def test_select_product_no_inventory(self):
+        # without coins
         for product in ["cola", "chips", "candy"]:
             self.vending_machine.select_product(product)
             self.assertEqual(self.vending_machine.check_display(), "SOLD OUT")
+        # with coins
+        for quarters in range(1, 8):
+            self.vending_machine.accept_coin({"weight": 5.670, "diameter": 24.26, "thickness": 1.75})
+        for product in ["cola", "chips", "candy"]:
+            self.vending_machine.select_product(product)
+            self.assertEqual(self.vending_machine.check_display(), "SOLD OUT")
+
+    def test_accept_coins(self):
+        num_quarters = 7
+        num_dimes = 7
+        num_nickels = 7
+        for quarters in range(1, num_quarters+1):
+            self.vending_machine.accept_coin({"weight": 5.670, "diameter": 24.26, "thickness": 1.75})
             self.assertEqual(self.vending_machine.check_display(), "AMOUNT: ${0:.2f}".format(
-                0.25*quarters + 0.10*dimes + 0.05*nickles))
+                0.25*quarters))
+        for dimes in range(1, num_dimes+1):
+            self.vending_machine.accept_coin({"weight": 2.268, "diameter": 17.91, "thickness": 1.35})
+            self.assertEqual(self.vending_machine.check_display(), "AMOUNT: ${0:.2f}".format(
+                0.25*num_quarters + 0.10*dimes))
+        for nickles in range(1, num_nickels+1):
+            self.vending_machine.accept_coin({"weight": 5.000, "diameter": 21.21, "thickness": 1.95})
+            self.assertEqual(self.vending_machine.check_display(), "AMOUNT: ${0:.2f}".format(
+                0.25*num_quarters + 0.10*num_dimes + 0.05*nickles))
         self.vending_machine.return_coins()
         self.assertEqual(self.vending_machine.check_display(), "INSERT COIN")
 
@@ -44,6 +49,41 @@ class TestVending(unittest.TestCase):
         self.assertEqual(self.vending_machine.product_stock, {"cola": 6, "chips": 5, "candy": 4})
         self.vending_machine.add_product_stock({"cola": 2, "chips": 3, "candy": 4})
         self.assertEqual(self.vending_machine.product_stock, {"cola": 8, "chips": 8, "candy": 8})
+
+    def test_select_product_with_stock_no_coin(self):
+        self.vending_machine.add_product_stock({"cola": 2, "chips": 3, "candy": 4})
+        self.vending_machine.return_coins()
+        for product in [("cola", 1.0), ("chips", 0.5), ("candy", 0.65)]:
+            self.vending_machine.select_product(product[0])
+            self.assertEqual(self.vending_machine.check_display(), "PRICE: ${0:.2f}".format(product[1]))
+
+    def test_exact_change_only(self):
+        self.vending_machine.accept_coin({"weight": 5.670, "diameter": 24.26, "thickness": 1.75})
+        self.vending_machine.accept_coin({"weight": 5.670, "diameter": 24.26, "thickness": 1.75})
+        self.vending_machine.accept_coin({"weight": 5.670, "diameter": 24.26, "thickness": 1.75})
+        self.vending_machine.add_product_stock({"cola": 2, "chips": 3, "candy": 4})
+        self.vending_machine.select_product("candy")
+        self.assertEqual("EXACT CHANGE ONLY", self.vending_machine.check_display())
+
+    def test_make_change(self):
+        self.vending_machine.add_product_stock({"cola": 6, "chips": 5, "candy": 4})
+        num_quarters = 5
+        num_dimes = 1
+        num_nickels = 1
+        for quarters in range(1, num_quarters+1):
+            self.vending_machine.accept_coin({"weight": 5.670, "diameter": 24.26, "thickness": 1.75})
+            self.assertEqual(self.vending_machine.check_display(), "AMOUNT: ${0:.2f}".format(
+                0.25*quarters))
+        for dimes in range(1, num_dimes+1):
+            self.vending_machine.accept_coin({"weight": 2.268, "diameter": 17.91, "thickness": 1.35})
+            self.assertEqual(self.vending_machine.check_display(), "AMOUNT: ${0:.2f}".format(
+                0.25*num_quarters + 0.10*dimes))
+        for nickles in range(1, num_nickels+1):
+            self.vending_machine.accept_coin({"weight": 5.000, "diameter": 21.21, "thickness": 1.95})
+            self.assertEqual(self.vending_machine.check_display(), "AMOUNT: ${0:.2f}".format(
+                0.25*num_quarters + 0.10*num_dimes + 0.05*nickles))
+        self.vending_machine.select_product("cola")
+        self.assertEqual({"quarters": 1, "dimes": 1, "nickles": 1}, self.vending_machine.coins_to_return)
 
 
 if __name__ == '__main__':
